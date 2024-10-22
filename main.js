28c7410ec2150b2f4988e968ee8e00eb28415da6
STORYABLOK <160139354+STORYABLOK@users.noreply.github.com>
2024-10-22 08:55:58 -0300

    22/10
    22/10

---
+++ main.js
@@ -0,0 +1,5 @@
+function criarCartao(categoria,pergunta,resposta){
+    console.log(categoria,pergunta,resposta)
+}
+
+criarCartao("Biologia", "Qual a função da mitocondria?", "Produção de energia (ATP)")
