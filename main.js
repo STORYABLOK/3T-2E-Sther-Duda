a5c3003cf72746bb57df5d19cb1229aba07e198c
STORYABLOK <160139354+STORYABLOK@users.noreply.github.com>
2024-10-28 08:52:25 -0300

    28/10
    28/10

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function criarCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML = 'Isso é um teste'
+    container.appendChild(article)
 }
 
+
 criarCartao("Biologia", "Qual a função da mitocondria?", "Produção de energia (ATP)")
+
