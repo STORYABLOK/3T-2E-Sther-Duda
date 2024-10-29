52a93c88d85653ba36ab7c5caa305f44b58dab24
STORYABLOK <160139354+STORYABLOK@users.noreply.github.com>
2024-10-29 08:52:20 -0300

    29/10
    29/10

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ function criarCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'Isso é um teste'
+    article.innerHTML = `
+    <div class="cartao-conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao-conteudo-pergunta">
+            <p>${pergunta}</p>
+        </div>
+        <div class="cartao-conteudo-resposta">
+            <p>${resposta}</p>
+        </div>
+    </div>
+
+`
     container.appendChild(article)
 }
 
 
 criarCartao("Biologia", "Qual a função da mitocondria?", "Produção de energia (ATP)")
+criarCartao("História", "Oque foi a idade média?", "Ficou marcada pelo feudalismo, pela influência da Igreja, e pelas Cruzadas e Inquisição.")
 
