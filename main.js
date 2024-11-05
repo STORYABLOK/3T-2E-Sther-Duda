40c3275a5cbd264064ca435e56f9eb1896442e59
STORYABLOK <160139354+STORYABLOK@users.noreply.github.com>
2024-11-05 08:48:43 -0300

    05/11
    05/11

--- main.js
+++ main.js
@@ -20,4 +20,5 @@ function criarCartao(categoria,pergunta,resposta){
 
 criarCartao("Biologia", "Qual a função da mitocondria?", "Produção de energia (ATP)")
 criarCartao("História", "Oque foi a idade média?", "Ficou marcada pelo feudalismo, pela influência da Igreja, e pelas Cruzadas e Inquisição.")
-
+criarCartao("Arte", "Quem foi Gustave Courbet?", "Courbet foi um pintor francês pioneiro do estilo realista francês. Foi acima de tudo um pintor da vida camponesa de sua região.")
+criarCartao("Ed.Financeira", " Método 50-30-20?", "O método 50-30-20 consiste na divisão da renda do mês, sendo despesas fixas, essenciais e obrigatórias em 50%, despesas variáveis em 30%, e objetivo a ser conquistado em 20%.")
